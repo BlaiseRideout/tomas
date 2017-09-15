b3c8324b295b1e7601f21a32996608129a7d7a60
Blaise Ritchie <britchie@ellumus.com>
2017-09-15 14:59:19 -0700

    Added round settings configuration

--- static/js/tournament.js
+++ static/js/tournament.js
@@ -1,12 +1,60 @@
 $(function() {
-	var playersTemplate;
-	$.get("/static/mustache/players.mst", function(data) {
-		playersTemplate = data;
-		Mustache.parse(data);
-		$.getJSON("/players", function(data) {
-			$("#players").html(Mustache.render(playersTemplate, {players:data}));
-		});
-	});
+	var templates = {};
+	function renderTemplate(template, endpoint, selector, callback) {
+		if(templates[template] === undefined)
+			$.get("/static/mustache/" + template, function(data) {
+				Mustache.parse(data);
+				templates[template] = data;
+				renderTemplate(template, endpoint, selector, callback);
+			});
+		else
+			$.getJSON(endpoint, function(data) {
+				$(selector).html(Mustache.render(templates[template], data));
+				if(typeof callback === "function")
+					callback(data);
+			});
+	}
+	function updatePlayers() {
+		renderTemplate("players.mst", "/players", "#players");
+	}
+	function updateSettings() {
+		renderTemplate("settings.mst", "/settings", "#settings", function() {
+			$("#addround").click(function() {
+				$.post("/addround", function(data) {
+					if(data['status'] === "success")
+						updateSettings();
+				}, "json");
+			});
+			$(".deleteround").click(function() {
+				$.post("/deleteround", {'round':$(this).parent().data("roundid")}, function(data) {
+					if(data['status'] === "success")
+						updateSettings();
+					else
+						console.log(data);
+				}, "json");
+			});
+			var updateSetting = function() {
+				var round = $(this).parent().data("roundid");
+				var settings = {};
+				console.log($(this).val());
+				if($(this).attr('type') === "checkbox")
+					settings[$(this).data("colname")] = $(this).prop('checked')?1:0;
+				else
+					settings[$(this).data("colname")] = $(this).val();
+				console.log(settings);
+				$.post("/settings", {'round':round, 'settings':JSON.stringify(settings)}, function(data) {
+					if(data['status'] !== "success")
+						console.log(data);
+				}, "json");
 
+			}
+			$(".roundsetting").change(updateSetting).keyup(updateSetting);
+		});
+	}
+	function update() {
+		updatePlayers();
+		updateSettings();
+	}
+	update();
 	$("#tournament").tabs();
 });
--- tournament.py
+++ tournament.py
@@ -14,6 +14,39 @@ class PlayersHandler(handler.BaseHandler):
         with db.getCur() as cur:
             cur.execute("SELECT Players.Id, Players.Name, Countries.IOC, Association, Pools.Name FROM Players LEFT OUTER JOIN Countries ON Countries.Id = Players.Country LEFT OUTER JOIN Pools ON Players.Pool = Pools.Id")
             rows = [{"id": row[0], "name": row[1], "country": row[2], "association": row[3], "pool": row[4]} for row in cur.fetchall()]
-            return self.write(json.dumps(rows))
+            return self.write(json.dumps({'players':rows}))
 
+class AddRoundHandler(handler.BaseHandler):
+    def post(self):
+        with db.getCur() as cur:
+            cur.execute("INSERT INTO Rounds(Number) VALUES((SELECT COUNT(*) + 1 FROM Rounds))")
+            return self.write(json.dumps({'status':"success"}))
 
+class DeleteRoundHandler(handler.BaseHandler):
+    def post(self):
+        round = self.get_argument("round", None)
+        if round is None:
+            return self.write(json.dumps({'status':"error", 'message':"Please provide a round"}))
+        with db.getCur() as cur:
+            cur.execute("DELETE FROM Rounds WHERE Id = ?", (round,))
+            return self.write(json.dumps({'status':"success"}))
+
+class SettingsHandler(handler.BaseHandler):
+    def get(self):
+        with db.getCur() as cur:
+            cur.execute("SELECT Id, Number, Seed, SoftCut, Duplicates, Diversity, UsePools FROM Rounds")
+            rows = [{"id": row[0], "number": row[1], "seed": row[2], "softcut": row[3], "duplicates": row[4], "diversity": row[5], "usepools": row[6]} for row in cur.fetchall()]
+            return self.write(json.dumps({'rounds':rows}))
+    def post(self):
+        round = self.get_argument("round", None)
+        if round is None:
+            return self.write(json.dumps({'status':"error", 'message':"Please provide a round"}))
+        settings = self.get_argument("settings", None)
+        if settings is None:
+            return self.write(json.dumps({'status':"error", 'message':"Please provide a settings object"}))
+        settings = json.loads(settings)
+        print(settings)
+        with db.getCur() as cur:
+            for colname, val in settings.items():
+                cur.execute("UPDATE Rounds SET {0} = ?".format(colname), (val,))
+            return self.write(json.dumps({'status':"success"}))
