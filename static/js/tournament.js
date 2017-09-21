1214a3b5fb5f271a98775e54d008bc7ba64d9b9e
John Canning <jmcanning@alum.mit.edu>
2017-09-21 12:56:17 -0700

    Add sort control by column header in players list

--- static/js/tournament.js
+++ static/js/tournament.js
@@ -3,6 +3,87 @@ $(function() {
 	var countries, countriesSelect;
 	var algorithms, algorithmsSelect;
 	var showInactive = false;
+	var sortkeys = {}; /* Most recent sorting for each template */
+	/* Structure sortkeys[templatename] =
+	    {'table': list of rows (table) in data to be sorted,
+	     'keys': list of keyspecs: ['field', +1 or -1, 'str' or 'num']
+	    }  */
+	var sortkeys = {
+		'players.mst': {
+			'table': 'players',
+			'keys': [
+				['inactive', 1, 'num'],
+				['name', 1, 'str'],
+				['country', 1, 'str']
+			]
+		},
+		'leaderboard.mst': {
+			'table': 'leaderboard',
+			'keys': [
+				['inactive', 1, 'num'],
+				['games_played', -1, 'num'],
+				['score', -1, 'num']
+			]
+		}
+	};
+
+	/* Build a comparison function that compares keys in order specified */
+	function compareFunc(keyspecs) {
+		if (keyspecs.length > 0) {
+			var spec = keyspecs[0],
+				inner = compareFunc(keyspecs.slice(1));
+			return function(a, b) {
+				if (spec[2] == 'str') {
+					if (a[spec[0]] < b[spec[0]]) {
+						return -1 * spec[1]
+					}
+					else if (a[spec[0]] > b[spec[0]]) {
+						return spec[1]
+					}
+					else {
+						return (inner && inner(a, b)) || 0
+					}
+				}
+				else {
+					if (a[spec[0]] - b[spec[0]] == 0) {
+						return (inner && inner(a, b)) || 0
+					}
+					else {
+						return (a[spec[0]] - b[spec[0]]) * spec[1]
+					}
+				}
+			}
+		}
+	}
+
+	function updateSortKeys(template, fieldname, fieldtype, table, callback) {
+		var newkey = [fieldname, 1, fieldtype];
+		if (!table && template.indexOf('.mst') > 0) {
+			table = template.slice(0, -4);
+		}
+		if (sortkeys[template] && sortkeys[template]['keys'] &&
+			sortkeys[template]['keys'][0][0] == fieldname) {
+			sortkeys[template]['keys'][0][1] *= -1
+		}
+		else if (sortkeys[template]) {
+			var keys = sortkeys[template]['keys'] || [];
+			for (var j = 1; j < keys.length; j++) {
+				if (keys[j][0] == fieldname) break;
+			}
+			if (j < keys.length) {
+				newkey = keys[j];
+				keys = keys.slice(0, j).concat(keys.slice(j + 1));
+			}
+			sortkeys[template]['keys'] = [newkey].concat(keys);
+		}
+		else {
+			sortkeys[template] = {
+				'table': table,
+				'keys': [newkey]
+			};
+		};
+		if (typeof callback === "function") callback();
+	}
 
 	function renderTemplate(template, endpoint, selector, callback, extra) {
 		if (templates[template] === undefined)
@@ -16,6 +97,11 @@ $(function() {
 				for (k in extra) {
 					data[k] = extra[k]
 				};
+				if (sortkeys[template]) {
+					var toSort = data[sortkeys[template]['table']];
+					toSort.sort(compareFunc(sortkeys[template]['keys']));
+					data[sortkeys[template]['table']] = toSort
+				}
 				$(selector).html(Mustache.render(
 					templates[template], data));
 				if (typeof callback === "function")
@@ -106,6 +192,13 @@ $(function() {
 			$(".addplayerbutton").click(addNewPlayer);
 			$(".playerfield[data-colname='Inactive']").click(
 				togglePlayerActiveStatus);
+			$(".colheader").click(function() {
+				updateSortKeys("players.mst",
+					$(this).data("fieldname"),
+					$(this).data("type"),
+					"players",
+					updatePlayers);
+			});
 			$("#showinactive").click(showHideInactive);
 			showHideInactive();
 		}, {
