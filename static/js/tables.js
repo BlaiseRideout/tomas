b540ecacb4dad59c083a78c5b8f580ad7596c0e6
John Canning <jmcanning@alum.mit.edu>
2017-09-26 13:11:30 -0700

    Enable user feedback on score editing in seating panel
    As authenticated users enter scores in the seating panel, the score
    entry boxes and the total score are highlighted as "bad" if they
    do not total 100,000 or 0.  Once they total 100,000, the rank and
    final scores are computed and displayed.

--- static/js/tables.js
+++ static/js/tables.js
@@ -1,22 +1,69 @@
 $(function() {
-	if(window.currentTab !== undefined)
+	if (window.currentTab !== undefined)
 		$("#seating").tabs({
-			active:currentTab
+			active: currentTab
 		});
 	else
 		$("#seating").tabs();
+
+	function round(num, digits) {
+		var d10 = Math.pow(10, digits),
+			shift = Math.round(num * d10),
+			sig = '' + shift,
+			n = sig.length;
+		return (shift % d10 == 0) ? '' + num :
+			sig.substring(0, n - digits) + '.' + sig.substring(n - digits);
+	};
+
 	function scoreChange() {
 		var score = $(this).val();
 		var player = $(this).parents(".player");
 		var table = player.parents(".table");
+		var tabletotal = table.prev("thead").find(".tabletotal");
 		var scores = table.find(".playerscore");
-		var total = 0;
+		var total = 0,
+			umas = [15, 5, -5, -15];
 		scores.each(function(i, elem) {
-			total += parseInt($(elem).val());
+			var val = parseInt($(elem).val());
+			total += val;
 		});
-		table.prev("thead").find(".tabletotal").text("TOTAL " + total);
+		tabletotal.text("TOTAL " + total);
+		newstate = (total == 100000 || total == 0) ? "good" : "bad";
+		delstate = (total == 100000 || total == 0) ? "bad" : "good";
+		table.find(".playerscore, .playerchombos").removeClass(delstate);
+		table.find(".playerscore, .playerchombos").addClass(newstate);
+		tabletotal.removeClass(delstate);
+		tabletotal.addClass(newstate);
+		if (total == 100000) {
+			var tablescore = [],
+				gameid = table.data("roundid") * 1000 + table.data("tableid");
+			table.find(".player").each(function() {
+				tablescore = tablescore.concat({
+					'gameid': gameid,
+					'playerid': $(this).data("playerid"),
+					'rawscore': parseInt($(this).find(".playerscore").val()),
+					'chombos': parseInt($(this).find(".playerchombos").val()),
+					'rank': $(this).find(".rank"),
+					'score': $(this).find(".score")
+				});
+			});
+			tablescore.sort(function(ra, rb) {
+				/* Sort by raw score descending, and chombos ascending */
+				return rb['rawscore'] == ra['rawscore'] ?
+					ra['chombos'] - rb['chombos'] :
+					rb['rawscore'] - ra['rawscore'];
+			});
+			for (var j = 0; j < tablescore.length; j++) {
+				tablescore[j]['rank'].text(j + 1);
+				tablescore[j]['rank'] = j + 1;
+				var score = tablescore[j]['rawscore'] / 1000.0 - 25 +
+					tablescore[j]['chombos'] * -8 + umas[j];
+				tablescore[j]['score'].text(round(score, 1));
+				tablescore[j]['score'] = score;
+			}
+		}
 	}
-	$(".playerscore").change(scoreChange).keyup(scoreChange);
+	$(".playerscore, .playerchombos").change(scoreChange).keyup(scoreChange);
 	$(".genround").click(function() {
 		var round = $(this).parents(".round").data("round");
 		$.post("/seating", {
