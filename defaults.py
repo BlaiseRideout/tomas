3028bca82c9aa9b21add672d57e0ba98648f8d91
John Canning <jmcanning@alum.mit.edu>
2017-12-20 16:37:38 -0800

    Add PROXYPREFIX setting for URL construction inside proxied sites
    Also added tournament name to the base template so it appears at
    the top of each page.

--- defaults.py
+++ defaults.py
@@ -12,6 +12,9 @@ DBBACKUPS = "backups"
 DBDATEFORMAT = "%Y-%m-%d-%H-%M-%S"
 
 # PREFERENCES
+# Tournament related
+#   DEFFAULTCUTSIZE controls the default number of players that advance
+#   between rounds.
 DEFAULTCUTSIZE = 32
 #   MAXSWAPDISTANCE is the maximum difference in rank that will
 #   be swapped to resolve duplicates and diversity
@@ -19,6 +22,12 @@ MAXSWAPDISTANCE = 10
 #   DUPLICATEIMPORTANCE is the factor by which duplicate resolution
 #   is prioritized over diversity resolution
 DUPLICATEIMPORTANCE = 4
+#   UNUSEDPOINTSINCREMENT is smallest amount of points that can be marked
+#   as unused at the end of game.  The unused points must be a multiple of
+#   this increment, typically 1000 for a riichi bet.
+UNUSEDPOINTSINCREMENT = 1000
+
+# Application behavior
 #   LINKVALIDDAYS is the number of days links for invitations and
 #   password resets should remain valid.  They expire after LINKVALIDDAYS
 #   has passed.
@@ -26,10 +35,10 @@ LINKVALIDDAYS = 7
 #   STATSHISTORYSIZE is the number of players who will be displayed
 #   in the "Recently Viewed" section of the player stats page
 STATSHISTORYSIZE = 10
-#   UNUSEDPOINTSINCREMENT is smallest amount of points that can be marked
-#   as unused at the end of game.  The unused points must be a multiple of
-#   this increment, typically 1000 for a riichi bet.
-UNUSEDPOINTSINCREMENT = 1000
+#   PROXYPREFIX is the URL prefix needed for users accessing this web server
+#   through a proxy server.  If this web server is being accessed directly,
+#   leave this as '/' or empty ''.
+PROXYPREFIX = '/'
 
 # EMAIL
 #   These settings are for the outbound email server that sends invites
