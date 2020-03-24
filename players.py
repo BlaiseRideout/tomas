ed027848dbe3d233bcf0e4e8ce8cd6f81fe6e6c6
John Canning <jmcanning@alum.mit.edu>
2020-03-24 09:23:21 -0700

    Start on endpoints to handle merge player records

--- players.py
+++ players.py
@@ -7,6 +7,7 @@ import io
 import logging
 import datetime
 import sqlite3
+from collections import *
 
 import tornado.web
 import handler
@@ -16,6 +17,16 @@ import settings
 import util
 
 log = logging.getLogger('WebServer')
+
+fetchValidPlayersSQL = """
+SELECT {columns} FROM Players
+ JOIN Countries ON Players.Country = Countries.Id
+ LEFT OUTER JOIN Compete ON Players.Id = Compete.Player
+ LEFT OUTER JOIN Tournaments ON Compete.Tournament = Tournaments.Id
+ LEFT OUTER JOIN Scores ON Players.Id = Scores.PlayerId
+ WHERE Players.ReplacedBy ISNULL {conditions}
+ GROUP BY Players.Id ORDER BY lower(Players.Name)
+"""
 
 def playerColumns():
     player_fields = [f for f in db.table_field_names('Players')
@@ -39,20 +50,12 @@ class PlayersListHandler(handler.BaseHandler):
         columns, colnames, colheads = playerColumns()
         try:
             with db.getCur() as cur:
-                cur.execute(
-                    "SELECT {columns} FROM Players"
-                    " JOIN Countries ON Players.Country = Countries.Id"
-                    " LEFT OUTER JOIN Compete ON Players.Id = Compete.Player"
-                    " LEFT OUTER JOIN Tournaments"
-                    "   ON Compete.Tournament = Tournaments.Id"
-                    " LEFT OUTER JOIN Scores ON Players.Id = Scores.PlayerId"
-                    " WHERE Players.ReplacedBy ISNULL"
-                    " GROUP BY Players.Id"
-                    " ORDER BY Players.Name".format(
-                        columns=",".join(columns)))
+                sql = validPlayersSQL.format(
+                    columns=",".join(columns), conditions="")
+                args = []
+                cur.execute(sql, args)
                 players = [dict(zip(colnames, row)) for row in cur.fetchall()]
-                result = {'status': 0, 
-                          'data': players, 'itemsCount': len(players)}
+                result = {'status': 0, 'data': players}
         except Exception as e:
             result = {'status': 1,
                       'message': 'Unable to get players from database. ' +
@@ -61,6 +64,7 @@ class PlayersListHandler(handler.BaseHandler):
         
     @tornado.web.authenticated
     def post(self):
+        global sql, args
         encoded_item = self.get_argument('item', None)
         item = json.loads(encoded_item)
         result = {'status': 0, 'message': ''}
@@ -136,6 +140,7 @@ class PlayersListHandler(handler.BaseHandler):
         self.write(result)
 
 def cleanPlayerItem(item, columns):
+    global sql, args
     for field in ['Name', 'Association']:
         if field in columns:
             item[field] = item[field].strip()
@@ -147,3 +152,94 @@ def cleanPlayerItem(item, columns):
             result = cur.fetchone()
             if result:
                 item['Country'] = result[0]
+
+class MergePlayersHandler(handler.BaseHandler):
+    
+    @tornado.web.authenticated
+    def post(self):
+        global sql, args
+        encoded_request = self.get_argument('request', None)
+        request = json.loads(encoded_request)
+        result = {'status': 0, 'message': ''}
+        if not (isinstance(request, dict) and 'playerIDs' in request):
+            result['message'] = 'Invalid request to merge players, {}'.format(
+                request)
+            result['status'] = -1
+        elif not (isinstance(request['playerIDs'], list) and
+                  all(map(lambda x: isinstance(x, int) and x > 0,
+                          request['playerIDs'])) and
+                  len(request['playerIDs']) > 1):
+            result['message'] = 'Invalid Player IDs in request, {}'.format(
+                request['playerIDs'])
+            result['status'] = -2
+        else:
+            columns, colnames, colheads = playerColumns()
+            try:
+                with db.getCur() as cur:
+                    sql = validPlayersSQL.format(
+                        columns=",".join(columns), 
+                        conditions='AND Players.Id IN ({})'.format(
+                            ",".join(map(str, playerIDs))))
+                    args = playerIDs
+                    cur.execute(sql, args)
+                    players = [dict(zip(colnames, row)) 
+                               for row in cur.fetchall()]
+                    missing = set(playerIDs) - set(p['Id'] for p in players)
+                    if len(missing) > 0:
+                        result['message'] = 'Missing Player IDs, {}'.format(
+                            missing)
+                        result['status'] = -3
+                    else:
+                        merge_record = self.combine_player_records(
+                            players, colnames)
+                        result['merged'] = merge_record
+                        if 'performMerge' in request and request['peformMerge']:
+                            self.merge_player_records(
+                                playerIDs, merge_record, colnames, cur, result)
+            except Exception as e:
+                result['message'] = (
+                    'Exception in database query. SQL = {}. Args = {}. {}'
+                    .format(sql, args, e))
+                log.error(result['message'])
+                result['status'] = -10
+            
+        self.write(result)
+
+    def combine_player_records(players, colnames):
+        res = {}
+        maxitem = 0
+        for col in colnames: # For each column, histogram values
+            hist = defaultdict(lambda: 0)
+            for player in players:
+                val = "" if player[col] is None else player[col]
+                hist[val] += 1
+            # Sort possible values by non-null flag, number of occurrences,
+            # string length or numeric value, # of uppercase letters
+            metrics = [(val, 1 if len(str(val)) > 0 else 0, hist[val],
+                       val if isinstance(val, int) else len(val),
+                       sum(1 if c.isupper() else 0 for c in val) 
+                       if isinstance(val, str) else 0)
+                      for val in hist]
+            metrics.sort(key=lambda tupl: tupl[1:])
+            res[col] = metrics[-1][0]  # Take highest after sort
+        return res
+
+    def merge_player_records(playerIDs, merge_record, colnames, cur, result):
+        global sql, args
+        keys = list(merge_record.keys())
+        sql = 'INSERT INTO Players ({}) VALUES ({})'.format(
+            ', '.join(keys), ', '.join(['?' for k in keys]))
+        args = [merge_record[k] for k in keys]
+        cur.execute(sql, args)
+        merge_reocrd['Id'] = cur.lastrowid
+        for table, field in (
+                ('Players', 'ReplacedBy'), ('Scores', 'PlayerId'), 
+                ('Compete', 'Player'), ('Seating', 'Player')):
+            sql = ('UPDATE {table} SET ({field} = {mergeID})'
+                   '  WHERE {field} in ({playerIDs})'.format(
+                       table=table, field=field, mergeID=merge_record['Id'],
+                       playerIDs=', '.join(str(id) for id in playerIDs)))
+            cur.execute(sql)
+        result['status'] = 0
+        result['message'] = '{} player records merged into ID {}'.format(
+            len(playerIDs), merge_record['Id'])
