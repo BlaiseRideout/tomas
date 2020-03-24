39f07e3137bd29ff7ff28b85c6c9ce93b6705876
John Canning <jmcanning@alum.mit.edu>
2020-03-23 19:20:40 -0700

    Add GUI controls for player merge operation

--- static/js/playerlist.js
+++ static/js/playerlist.js
@@ -2,6 +2,19 @@ $(function() {
     $.getJSON("/countries", function(countryList) {
 	var anyCountry = [{Code: "any", Id: NaN}],
 	    selectedPlayers = new Object(), // hash of selected Player Id's
+	    createMergeButton = function(item) {
+		// merge icon: triple nested greater than ⫸ (u-2AF8)
+		return $("<input>").addClass(
+		    "player-merge-button").attr({
+			type: "button",
+			title: "Merge selected players",
+		    }).data("playerID", item.Id)
+		    .css("display", "none").text("⫸").on(
+			"click", function(e) {
+			mergeSelectedPlayers(selectedPlayers, e);
+			e.stopPropagation();
+		    })
+	    },
 	    fieldDescriptions = [
 		{ name: "Id", type: "number", width: 5, visible: false },
 		{ name: "Name", type: "text", width: 150, validate: "required"},
@@ -17,13 +30,15 @@ $(function() {
 		{ name: "Latest", type: "text", width: 50,
 		  editing: false, inserting: false },
 		{ name: "", type: "checkbox", width: 10,
-		  inserting: false,
+		  inserting: false, css: "PlayerSelectBox",
 		  itemTemplate: function(value, item) {
 		      return this._createCheckbox().prop({
 			  checked: item.Id && selectedPlayers[item.Id],
 			  disabled: false
 		      }).click(function (e) {
 			  selectedPlayers[item.Id] = $(this).prop("checked");
+			  players = selectedPlayerIDs();
+			  updatePlayerMergeButtons();
 			  e.stopPropagation();
 		      });
 		  },
@@ -41,13 +56,49 @@ $(function() {
 			  $result = $result.add(this._createDeleteButton(item));
 		      }
 
+		      $result = $result.add(createMergeButton(item));
+
 		      return $result;
 		  }
 		}
 	],
 	    gridController = makeController(base + "playerslist/",
 					    fieldDescriptions);
+	
+	function selectedPlayerIDs() {
+	    var playerlist = [];
+	    for (p in selectedPlayers) {
+		if (selectedPlayers[p]) { playerlist.push(p) }};
+	    return playerlist;
+	};
+	
+	function clearSelectedPlayers() {
+	    for (p in selectedPlayers) { delete selectedPlayers[p] };
+	    $("#playersGrid .PlayerSelectBox input").prop("checked", false);
+	};
 
+	function updatePlayerMergeButtons() {
+	    $("#playersGrid .player-merge-button").each(
+		function(i, e) {
+		    var id = $(e).data("playerID");
+		    $(e).css("display",
+			     players.length > 1 && selectedPlayers[id] ?
+			     "inline" : "none")
+		});
+	};
+	
+	function mergeSelectedPlayers(selectedPlayers, e) {
+	    players = selectedPlayerIDs();
+	    if (players.length < 2) {
+		$.notify('Merge requires 2 or more players')
+	    } else if (window.confirm(
+		'OK to merge ' + players.length + ' players?')) {
+		console.log('Merge players with IDs: ', players.join(', '));
+		clearSelectedPlayers();
+		updatePlayerMergeButtons();
+	    }
+	};
+	
 	$("#playersGrid").jsGrid({
             width: "100%",
             height: "auto",
