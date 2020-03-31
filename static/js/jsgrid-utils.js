c49a68bcdcb2f69692510bf11bcef0c1eb52e9c9
John Canning <jmcanning@alum.mit.edu>
2020-03-30 22:19:59 -0700

    Beautified js files from previous commits

--- static/js/jsgrid-utils.js
+++ static/js/jsgrid-utils.js
@@ -1,74 +1,88 @@
 (function() {
-    updateFlagImage = function(updateObj, countries) {
-	var country = countries.find( // Find country ID in countries array
-	    function (country) {return country.Id == updateObj.item.Country});
-	if (country) {
-	    updateObj.item['Flag'] = country.Flag_Image;
-	    $(updateObj.row).find("td.FlagImage").html(country.Flag_Image);
-	}
-    };
-    
-    makeFilter = function(filterItem, fieldDescriptions) {
-	return function(item) { // Create a function to filter items based on
-	    var keep = true; // user's filter parameters and field types
-	    for (field in fieldDescriptions) {
-		var fd = fieldDescriptions[field];
-		if (filterItem[fd.name] && ( // Only check visible fields
-		    !fd.hasOwnProperty("visible") || fd.visible)) {
-		    if ((["number", "checkbox", "select"].indexOf(fd.type) > -1
-			 && item[fd.name] != filterItem[fd.name]) ||
-			(fd.type == "text" && item[fd.name] &&
-			 item[fd.name].toLowerCase().indexOf(
-			     filterItem[fd.name].toLowerCase()) == -1) ) {
-			keep = false;
-			break;  // Once a filter fails, quit loop
-		    }
+	updateFlagImage = function(updateObj, countries) {
+		var country = countries.find( // Find country ID in countries array
+			function(country) {
+				return country.Id == updateObj.item.Country
+			});
+		if (country) {
+			updateObj.item['Flag'] = country.Flag_Image;
+			$(updateObj.row).find("td.FlagImage").html(country.Flag_Image);
 		}
-	    };
-	    return keep
-	}
-    };
-    
-    function postChange(jsonURL, item, deferred) {
-	$.post(jsonURL, {item: JSON.stringify(item)}, 
-	       function(resp) {
-		   if (resp.status != 0) { $.notify(resp.message) };
-		   deferred.resolve(resp.status == 0 ? resp.item : null);
-	       }, "json");
-    };
-    
-    makeController = function(jsonURL, fieldDescriptions) {
-	return { // Create a controller object for jsGrid
-	    loadData: function(filterItem) { // Load GETs jsonURL
-		var d = $.Deferred();
-		$.ajax({url: jsonURL, dataType: "json"}).done(
-		    function(loadData) { // Return has status = 0 for success
-			if (loadData.status != 0) {
-			    $.notify(loadData.message);
-			    d.resolve();
-			} else { var items = loadData.data.filter(
-			    makeFilter(filterItem, fieldDescriptions));
-			       d.resolve(items) }
-		    });
-		return d.promise();
-	    },	     
-	    insertItem: function(item) {
-		var d = $.Deferred();
-		item.Id = 0;  // Insert POSTs new item with ID == 0
-		postChange(jsonURL, item, d);
-		return d.promise();
-	    },
-	    updateItem: function(item) {
-		var d = $.Deferred(); // Update POSTs modified item (same ID)
-		postChange(jsonURL, item, d);
-		return d.promise();
-	    },
-	    deleteItem: function(item) {
-		var d = $.Deferred(); // Delete POSTs item with negative ID
-		if (item.Id) {item.Id = - item.Id};
-		postChange(jsonURL, item, d);
-		return d.promise();
-	    },
 	};
-    };
+
+	makeFilter = function(filterItem, fieldDescriptions) {
+		return function(item) { // Create a function to filter items based on
+			var keep = true; // user's filter parameters and field types
+			for (field in fieldDescriptions) {
+				var fd = fieldDescriptions[field];
+				if (filterItem[fd.name] && ( // Only check visible fields
+						!fd.hasOwnProperty("visible") || fd.visible)) {
+					if ((["number", "checkbox", "select"].indexOf(fd.type) > -1 &&
+							item[fd.name] != filterItem[fd.name]) ||
+						(fd.type == "text" && item[fd.name] &&
+							item[fd.name].toLowerCase().indexOf(
+								filterItem[fd.name].toLowerCase()) == -1)) {
+						keep = false;
+						break; // Once a filter fails, quit loop
+					}
+				}
+			};
+			return keep
+		}
+	};
+
+	function postChange(jsonURL, item, deferred) {
+		$.post(jsonURL, {
+				item: JSON.stringify(item)
+			},
+			function(resp) {
+				if (resp.status != 0) {
+					$.notify(resp.message)
+				};
+				deferred.resolve(resp.status == 0 ? resp.item : null);
+			}, "json");
+	};
+
+	makeController = function(jsonURL, fieldDescriptions) {
+		return { // Create a controller object for jsGrid
+			loadData: function(filterItem) { // Load GETs jsonURL
+				var d = $.Deferred();
+				$.ajax({
+					url: jsonURL,
+					dataType: "json"
+				}).done(
+					function(loadData) { // Return has status = 0 for success
+						if (loadData.status != 0) {
+							$.notify(loadData.message);
+							d.resolve();
+						}
+						else {
+							var items = loadData.data.filter(
+								makeFilter(filterItem, fieldDescriptions));
+							d.resolve(items)
+						}
+					});
+				return d.promise();
+			},
+			insertItem: function(item) {
+				var d = $.Deferred();
+				item.Id = 0; // Insert POSTs new item with ID == 0
+				postChange(jsonURL, item, d);
+				return d.promise();
+			},
+			updateItem: function(item) {
+				var d = $.Deferred(); // Update POSTs modified item (same ID)
+				postChange(jsonURL, item, d);
+				return d.promise();
+			},
+			deleteItem: function(item) {
+				var d = $.Deferred(); // Delete POSTs item with negative ID
+				if (item.Id) {
+					item.Id = -item.Id
+				};
+				postChange(jsonURL, item, d);
+				return d.promise();
+			},
+		};
+	};
 })();
