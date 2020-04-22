3cbb6c128ff9eebdbc9ef31ec53418daa1f05de3
John Canning <jmcanning@alum.mit.edu>
2020-04-22 13:10:34 -0700

    Make beautify.sh produce a summary of files changed

--- beautify.sh
+++ beautify.sh
@@ -1,13 +1,28 @@
-#!/bin/sh
+#!/bin/bash
 
 if [ $# -eq 0 ] ; then
-	for I in $(ls static/js/*.js); do
-		if [[ `echo "$I" | grep '\.min\.js$'` == "" ]] ; then
-			echo js-beautify -tnr --brace-style=end-expand "$I"
-			js-beautify -tnr --brace-style=end-expand "$I"
-		fi
+    changed=()
+    for I in $(ls static/js/*.js); do
+	if [[ `echo "$I" | grep '\.min\.js$'` == "" ]] ; then
+	    echo js-beautify -tnr --brace-style=end-expand "$I"
+	    result=`js-beautify -tnr --brace-style=end-expand "$I"`
+	    echo $result
+	    if [[ `echo "$result" | sed -n '/unchanged$/p'` == '' ]] ; then
+		changed=($changed $I)
+	    fi
+	fi
+    done
+    if [ ${#changed[*]} -eq 0 ] ; then
+	echo 'No files changed'
+    else
+	echo -n ${#changed[*]} File
+	if [ ${#changed[*]} -gt 1 ] ; then echo -n s ; fi
+	echo ' changed:'
+	for file in ${changed[*]} ; do
+	    echo " " $file
 	done
+    fi
 else
-	js-beautify -tnr --brace-style=end-expand $*
+    js-beautify -tnr --brace-style=end-expand $*
 fi
 
